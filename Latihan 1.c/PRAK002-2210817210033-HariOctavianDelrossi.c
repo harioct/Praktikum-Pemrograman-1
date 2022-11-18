ad86233bffe4d468091c7769cba9aa0790fc8892
harioct <114865767+harioct@users.noreply.github.com>
2022-11-18 21:05:34 +0700

    Delete Latihan 1.c directory

--- Latihan 1.c/PRAK002-2210817210033-HariOctavianDelrossi.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Selamat Pagi, Hari Octavian Delrossi\n");
-    printf("Selamat Siang, Hari Octavian Delrossi\n");
-    printf("Selamat Malam, Hari Octavian Delrossi\n");
-    return 0;
-}
